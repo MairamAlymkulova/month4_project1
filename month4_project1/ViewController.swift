4ffb0ada57002aa22263b42f83ab8f809723dfcd
MairamAlymkulova <mayam.a2003@gmail.com>
2023-09-02 15:23:20 +0600

    Initial Commit

---
+++ month4_project1/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  month4_project1
+//
+//  Created by User1 on 2/9/23.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
